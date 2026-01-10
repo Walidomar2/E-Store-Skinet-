d81e18be070cd7b0697901c1e367e285e16c9fd3
Walidomar2 <walidomar291@gmail.com>
2026-01-10 16:01:40 +0200

    Initial backend project

---
+++ Application Backend/Skinet-Store/Infrastructure/Data/ApplicationContext.cs
@@ -0,0 +1,29 @@
+﻿using Core.Entities;
+using Infrastructure.Config;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Data
+{
+    public class ApplicationContext : DbContext
+    {
+        //Db sets
+        public DbSet<Product> Products { get; set; }
+
+        public ApplicationContext(DbContextOptions<ApplicationContext> options): base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(ProductConfiguration).Assembly);
+        }
+
+    }
+}
